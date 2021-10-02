7dd5f5bb134adbcb18f79a997c5efede8b9c1b46
Ayush <aksayushx@gmail.com>
2021-10-02 23:17:51 +0530

    final updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,27 @@
-import Carousel from 'react-bootstrap/Carousel';
-import logo from '../assets/logo.png';
-import slide1 from '../assets/slide1.png';
-import slide2 from '../assets/slide2.png';
-import appStore from '../assets/AppStoreBadge.svg';
-import playStore from '../assets/google-play-badge.png';
-import { Icons } from './icons';
-import React from 'react';
-import { Navbar } from './navbar';
-import '../styles/App.css';
-import { useQuery, gql } from '@apollo/client';
-
+import Carousel from "react-bootstrap/Carousel";
+import logo from "../assets/logo.png";
+import slide1 from "../assets/slide1.png";
+import slide2 from "../assets/slide2.png";
+import appStore from "../assets/AppStoreBadge.svg";
+import playStore from "../assets/google-play-badge.png";
+import { Icons } from "./icons";
+import React from "react";
+import { Navbar } from "./navbar";
+import "../styles/App.css";
+import { useQuery, gql } from "@apollo/client";
 
 function App() {
+  /*const FEED_QUERY = gql`
+    {
+      title
+      subtitle
+    }
+  `;
 
-  const FEED_QUERY = gql`
-  {
-    title
-    subtitle
-  }
-`;
-
-  const { data } = useQuery(FEED_QUERY);
+  const { data } = useQuery(FEED_QUERY);*/
   //console.log(data.title);
-  const desc = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+  const desc =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
   return (
     <div className="App">
       <div className="container-fluid">
@@ -35,16 +34,8 @@ function App() {
                 </a>
               </div>
             </nav>
-            <h2 id="head-left">{data && (
-              <>
-                {data.title}
-              </>
-            )}</h2>
-            <p id="para">{data && (
-              <>
-                {data.subtitle}
-              </>
-            )}</p>
+            <h2 id="head-left">{desc}</h2>
+            <p id="para">{desc}</p>
             <div className="row download">
               <img src={appStore} alt="AppStore" />
               <img src={playStore} alt="PlayStore" />
@@ -54,11 +45,7 @@ function App() {
             <Navbar className="my-nav" />
             <Carousel className="my-carousel">
               <Carousel.Item>
-                <img
-                  className="slide"
-                  src={slide1}
-                  alt="First slide"
-                />
+                <img className="slide" src={slide1} alt="First slide" />
                 <Carousel.Caption>
                   <div className="container caption-carousel">
                     <h5 className="head align-left">Type Text</h5>
@@ -67,11 +54,7 @@ function App() {
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img
-                  className="slide"
-                  src={slide2}
-                  alt="Second slide"
-                />
+                <img className="slide" src={slide2} alt="Second slide" />
                 <Carousel.Caption>
                   <div className="container caption-carousel">
                     <h6 className="head align-left">Type Text</h6>
